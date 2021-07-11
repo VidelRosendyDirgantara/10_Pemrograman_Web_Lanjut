84678142da40175c3b0ba4c6d02becb4a2feb819
VidelRosendyDirgantara <videl2706@gmail.com>
2021-07-11 16:56:12 +0700

    Edit Route Web

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ArticleController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('articles', ArticleController::class);
+Route::get('/articles', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
